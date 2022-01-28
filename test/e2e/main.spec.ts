ac0d77703b681b2b6a1130599530b6a20106c0de
Kevin Hillinger <kevin.hillinger@microsoft.com>
2022-01-28 15:18:06 -0500

    testing scaffolding with jest

---
+++ test/e2e/main.spec.ts
@@ -0,0 +1,20 @@
+
+
+// TODO: fill in e2e placeholder
+
+describe('main end to end test', () => {
+    beforeAll(async () => {
+        // do something before each test
+    })
+    afterAll(async () => {
+        // tear down or do something after each test executes
+    })
+
+    describe('main()', () => {
+        // TODO: this should be more descriptive. this is just a placeholder
+        it('should execute successfully', async () => {
+        
+            expect("").not.toBeNull()
+        })
+    })
+});
